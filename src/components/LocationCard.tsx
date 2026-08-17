bd153261b2cda0169ffd6148fa49d9e2a2611a8f
S3kk0um1 <s.sekkoumi8@gmail.com>
2026-08-17 07:40:42 +0200

    Update LocationCard with image and vertical layout

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,20 +1,45 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 type LocationCardProps = {
     name: string;
     rating: number;
+    imageUrl : string
 };
 
-export default function LocationCard(props : LocationCardProps){
+export default function LocationCard({name,rating,imageUrl} : LocationCardProps){
     
     return (
-        <View>
-            <Text>
-                {props.name}
-            </Text>
-            <Text>
-                {props.rating}
-            </Text>
+        
+        <View style={styles.card}>
+            <Image style={styles.image} source={{uri : imageUrl}}>
+                
+            </Image>
+            <View style={styles.infoLocation}>
+                <Text>
+                    {name}
+                </Text>
+                <Text>
+                    {rating}
+                </Text>
+            </View>
+            
+            
         </View>
-
     )
-}
+    
+};
+
+const styles = StyleSheet.create({
+    card : {
+        
+        justifyContent: 'space-between',
+        backgroundColor: '#eeeeee',   
+        padding: 16,
+    },
+    image : {
+        width : '100%',
+        height : 200
+    },
+    infoLocation:{
+        flexDirection: 'row'
+    }
+})
